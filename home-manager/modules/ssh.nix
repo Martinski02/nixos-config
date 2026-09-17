93c7143995992461adf2097570bb450f0621959c
Martinski02 <214127725+Martinski02@users.noreply.github.com>
2026-09-17 19:24:25 +0000

    feat: add SSH client configuration

--- home-manager/modules/ssh.nix
+++ home-manager/modules/ssh.nix
@@ -1,4 +1,27 @@
 { ... }:
 
 {
+  programs.ssh = {
+    enable = true;
+
+    matchBlocks = {
+      "github.com" = {
+        hostname = "github.com";
+        user = "git";
+
+        identityFile = "~/.ssh/id_ed25519";
+        identitiesOnly = true;
+        addKeysToAgent = "yes";
+      };
+
+      "martin-server" = {
+        hostname = "martin-server";
+        user = "martin";
+
+        identityFile = "~/.ssh/id_ed25519";
+        identitiesOnly = true;
+        addKeysToAgent = "yes";
+      };
+    };
+  };
 }
