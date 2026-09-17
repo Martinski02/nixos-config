916425f2be1ef45587adb65edde897e3b855ce86
Martinski02 <214127725+Martinski02@users.noreply.github.com>
2026-09-17 18:21:59 +0000

    feat: add desktop applications and utilities

--- home-manager/modules/desktop.nix
+++ home-manager/modules/desktop.nix
@@ -1,4 +1,17 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
+  programs.firefox.enable = true;
+
+  home.packages = with pkgs; [
+    nextcloud-client
+    onlyoffice-desktopeditors
+    pavucontrol
+    tree
+    wl-clipboard
+
+    kdePackages.dolphin
+    kdePackages.ark
+    kdePackages.kio-extras
+  ];
 }
