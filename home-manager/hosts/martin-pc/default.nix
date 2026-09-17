19712be4fbb0b556df25f0368f47f80049c1befb
Martinski02 <214127725+Martinski02@users.noreply.github.com>
2026-09-17 19:17:17 +0000

    feat: define PC home data paths

--- home-manager/hosts/martin-pc/default.nix
+++ home-manager/hosts/martin-pc/default.nix
@@ -1,7 +1,33 @@
-{ ... }:
+{ config, ... }:
 
 {
   imports = [
     ./hyprland.nix
   ];
+
+  home.file = {
+    "documents".source =
+      config.lib.file.mkOutOfStoreSymlink "/mnt/data/media/documents";
+
+    "pictures".source =
+      config.lib.file.mkOutOfStoreSymlink "/mnt/data/media/pictures";
+
+    "videos".source =
+      config.lib.file.mkOutOfStoreSymlink "/mnt/data/media/videos";
+
+    "games".source =
+      config.lib.file.mkOutOfStoreSymlink "/mnt/data/games";
+
+    "nextcloud".source =
+      config.lib.file.mkOutOfStoreSymlink "/mnt/data/media/documents/nextcloud";
+  };
+
+  xdg.userDirs = {
+    enable = true;
+    createDirectories = false;
+
+    documents = "${config.home.homeDirectory}/documents";
+    pictures = "${config.home.homeDirectory}/pictures";
+    videos = "${config.home.homeDirectory}/videos";
+  };
 }
