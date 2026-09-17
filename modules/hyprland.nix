e341e83075f4ee073831ee5d412c24e7b2bdb2c0
Martinski02 <214127725+Martinski02@users.noreply.github.com>
2026-09-17 18:24:10 +0000

    feat: add Hyprland system integration

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -1,4 +1,41 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
+  services.xserver = {
+    enable = true;
+
+    xkb = {
+      layout = "de";
+      variant = "";
+    };
+  };
+
+  services.displayManager.sddm.enable = true;
+
+  programs.hyprland = {
+    enable = true;
+    withUWSM = true;
+    xwayland.enable = true;
+  };
+
+  xdg.portal = {
+    enable = true;
+
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+    ];
+  };
+
+  security.rtkit.enable = true;
+
+  services.pipewire = {
+    enable = true;
+
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
+
+    pulse.enable = true;
+  };
 }
