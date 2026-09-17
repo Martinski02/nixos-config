406bc016ddf0837ee6a6b0d5288dc0b04ce40306
Martinski02 <214127725+Martinski02@users.noreply.github.com>
2026-09-17 19:27:54 +0000

    feat: configure Git identity

--- home-manager/modules/git.nix
+++ home-manager/modules/git.nix
@@ -1,5 +1,14 @@
 { ... }:
 
 {
-  programs.git.enable = true;
+  programs.git = {
+    enable = true;
+
+    settings = {
+      user = {
+        name = "Martinski02";
+        email = "214127725+Martinski02@users.noreply.github.com";
+      };
+    };
+  };
 }
