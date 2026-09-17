3b9871e2e9482bc89b208f2f68952831037415f5
Martinski02 <214127725+Martinski02@users.noreply.github.com>
2026-09-17 18:10:22 +0000

    feat: add base system configuration

--- modules/base.nix
+++ modules/base.nix
@@ -1,4 +1,18 @@
 { ... }:
 
 {
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  users.users.martin = {
+    isNormalUser = true;
+    home = "/home/martin";
+    extraGroups = [
+      "wheel"
+    ];
+  };
+
+  system.stateVersion = "26.05";
 }
