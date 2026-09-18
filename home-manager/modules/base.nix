9935cf6c838b00b150a1fd9c9ea16939badc8cff
Martinski02 <214127725+Martinski02@users.noreply.github.com>
2026-09-18 16:58:24 +0200

    feat: add atuin shell history

--- home-manager/modules/base.nix
+++ home-manager/modules/base.nix
@@ -1,4 +1,16 @@
 { ... }:
 
 {
+  programs.bash.enable = true;
+
+  programs.atuin = {
+    enable = true;
+    enableBashIntegration = true;
+
+    settings = {
+      auto_sync = false;
+      update_check = false;
+      secrets_filter = true;
+    };
+  };
 }
