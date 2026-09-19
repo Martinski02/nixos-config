77d7236e77cea3ef7eace554dae78f3698d55519
Martinski02 <214127725+Martinski02@users.noreply.github.com>
2026-09-19 15:52:56 +0200

    Harden SSH access on martin-pc

--- hosts/martin-pc/ssh.nix
+++ hosts/martin-pc/ssh.nix
@@ -1,4 +1,11 @@
 { ... }:
 
 {
+  users.users.martin.openssh.authorizedKeys.keys = [
+    # martin-laptop
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBuliE+0C4H7r/J/ND6pMFdRyXOZjbfHiWjTHXByvsTo martin-laptop"
+
+    # martin-server
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGybPGy3NZfC14Dek8N7kj/RXB4x4tbroKh6Mq4rnbMt martin-server"
+  ];
 }
--- modules/ssh.nix
+++ modules/ssh.nix
@@ -6,6 +6,12 @@
   services.openssh = {
     enable = true;
     openFirewall = false;
+
+    settings = {
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+      PermitRootLogin = "no";
+    };
   };
 
   networking.firewall.interfaces."tailscale0".allowedTCPPorts = [
