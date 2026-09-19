fce53cc8935c6664d91a7491e4c705372b35c75b
Martinski02 <214127725+Martinski02@users.noreply.github.com>
2026-09-19 16:44:36 +0200

    Configure VS Code user settings

--- home-manager/modules/vscode.nix
+++ home-manager/modules/vscode.nix
@@ -1,5 +1,23 @@
 { ... }:
 
 {
-  programs.vscode.enable = true;
+  programs.vscode = {
+    enable = true;
+
+    profiles.default.userSettings = {
+      "[python]" = {
+        "diffEditor.ignoreTrimWhitespace" = false;
+        "editor.defaultColorDecorators" = "never";
+        "editor.formatOnType" = true;
+        "editor.wordBasedSuggestions" = "off";
+      };
+
+      "explorer.confirmDelete" = false;
+      "editor.fontSize" = 16;
+      "workbench.startupEditor" = "none";
+
+      "chatgpt.followUpQueueMode" = "steer";
+      "chatgpt.composerEnterBehavior" = "cmdAlways";
+    };
+  };
 }
