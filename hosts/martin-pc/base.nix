04d575c063bdb5335693b4bddaad8755fd7c4c5a
Martinski02 <214127725+Martinski02@users.noreply.github.com>
2026-09-20 16:26:39 +0200

    Configure encrypted backup SSD

--- hosts/martin-pc/base.nix
+++ hosts/martin-pc/base.nix
@@ -29,10 +29,17 @@
 
   environment.etc."crypttab".text = ''
     cryptdata UUID=053529f8-77c7-4005-8660-e4c281d2522b /root/.keys/cryptdata.key luks
+    cryptbackup UUID=881e21ff-c13a-4799-8a58-a21178ab8b4b /root/.keys/cryptbackup.key luks,nofail
   '';
 
   fileSystems."/mnt/data" = {
     device = "/dev/mapper/cryptdata";
     fsType = "ext4";
+  };
+
+  fileSystems."/mnt/backup" = {
+    device = "/dev/mapper/cryptbackup";
+    fsType = "ext4";
+    options = [ "nofail" ];
   };
 }
