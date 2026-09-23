7101407c1ba843a20f13d03222edf489a04c9cfd
Martinski02 <214127725+Martinski02@users.noreply.github.com>
2026-09-24 01:34:41 +0200

    fix(laptop): use native Firefox Wayland

--- hosts/martin-laptop/base.nix
+++ hosts/martin-laptop/base.nix
@@ -13,6 +13,4 @@
   boot.initrd.luks.devices."luks-c8c509f3-bcb2-47c2-8cff-6fa91a0d5dcc".device =
     "/dev/disk/by-uuid/c8c509f3-bcb2-47c2-8cff-6fa91a0d5dcc";
 
-  # Existing laptop workaround. Re-evaluate after Hyprland is working.
-  environment.sessionVariables.MOZ_ENABLE_WAYLAND = "0";
 }
