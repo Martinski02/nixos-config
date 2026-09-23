1532c6c0aa310cd18ed5eaf923ae80656376e8c1
Martinski02 <214127725+Martinski02@users.noreply.github.com>
2026-09-23 21:31:10 +0200

    refactor: remove samba mount from shared networking

--- modules/networking.nix
+++ modules/networking.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
   networking.networkmanager.enable = true;
@@ -8,29 +8,5 @@
   services.tailscale = {
     enable = true;
     openFirewall = true;
-  };
-
-  environment.systemPackages = with pkgs; [
-    cifs-utils
-    samba
-  ];
-
-  fileSystems."/mnt/martin-server-shares" = {
-    device = "//martin-server/shares";
-    fsType = "cifs";
-
-    options = [
-      "credentials=/home/martin/.config/samba/martin-server-credentials"
-      "uid=1000"
-      "gid=100"
-      "iocharset=utf8"
-      "_netdev"
-      "nofail"
-      "noauto"
-      "x-systemd.automount"
-      "x-systemd.idle-timeout=60"
-      "x-systemd.device-timeout=5s"
-      "x-systemd.mount-timeout=5s"
-    ];
   };
 }
